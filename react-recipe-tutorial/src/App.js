ab7a5e4a42a8d23ecdcb5d9a55b1de0756a41d02
Dee Werner <dee.werner@MediaPro.com>
2020-10-20 07:43:39 -0700

    updates from completing tutorial

--- react-recipe-tutorial/src/App.js
+++ react-recipe-tutorial/src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import Recipe from './Recipe'
+import './App.css'
 
-function App() {
+const App = () => {
+  const APP_ID = '1eed5bac'
+  const APP_KEY = '3bf00a1dc576cb48372209d36a80fec1'
+
+  const [recipes, setRecipes] = useState([])
+  const [search, setSearch] = useState('')
+  const [query, setQuery] = useState('chicken')
+
+  useEffect(() => {
+    getRecipes()
+  }, [query])
+
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+    )
+    const data = await response.json()
+    setRecipes(data.hits)
+  }
+  /*
+  // below code used a default string from the edamam site
+  // which already had 'chicken' set in the URL as the search
+  // see code above for proper implementation - using state
+  // of query to pass search params
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+    )
+    const data = await response.json()
+    console.log(data.hits)
+    setRecipes(data.hits)
+  }
+*/
+  const updateSearch = (e) => {
+    setSearch(e.target.value)
+  }
+
+  const getSearch = (e) => {
+    e.preventDefault()
+    setQuery(search)
+    setSearch('')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <form onSubmit={getSearch} className='search-form'>
+        <input
+          className='search-bar'
+          type='text'
+          value={search}
+          onChange={updateSearch}
+        />
+        <button className='search-button' type='submit'>
+          Search
+        </button>
+      </form>
+      <div className='recipes'></div>
+      {recipes.map((recipe) => (
+        <Recipe
+          key={recipe.recipe.label}
+          title={recipe.recipe.label}
+          calories={recipe.recipe.calories}
+          image={recipe.recipe.image}
+          ingredients={recipe.recipe.ingredients}
+        />
+      ))}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
